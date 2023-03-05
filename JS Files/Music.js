229c31705a60e6a226ac8c7d369e57ff65f7bc32
Sybe-Developer <123803359+Sybe-Developer@users.noreply.github.com>
2023-03-05 09:52:38 +0530

    Add files via upload

---
+++ JS Files/Music.js
@@ -0,0 +1,13 @@
+function play(){
+    var audio = document.getElementById("audio");
+    audio.play();
+    document.getElementById("play").style.display="none";
+    document.getElementById("pause").style.display="inline-block";   
+}
+
+function pause(){
+    var audio = document.getElementById("audio");
+    audio.pause();
+    document.getElementById("play").style.display="inline-block";
+    document.getElementById("pause").style.display="none";
+}
