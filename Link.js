f70e17a6128ab99a2d700f480d61533c4a96446c
Sybe-Developer <123803359+Sybe-Developer@users.noreply.github.com>
2023-04-02 16:47:01 +0530

    Add files via upload

---
+++ Link.js
@@ -0,0 +1,19 @@
+function link1() {
+window.open("https://www.mediafire.com/file/ldpqoy7od2wmb1l/Translator.exe/file");
+}
+
+function link2() {
+window.open("https://www.mediafire.com/file/lwvy72ix43k6eow/rps.exe/file");
+}
+
+function link3() {
+window.open("https://sourceb.in/xsTkA3OtaR");
+}
+
+function link4() {
+window.open("https://sourceb.in/WztidG0BNE");
+}
+
+function link5(){
+    window.open("https://sourceb.in/DFQEpqDyy7");
+}
