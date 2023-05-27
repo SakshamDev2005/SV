e175b438669f08990265e9fa113b96e4547ed657
Sybe-Developer <123803359+Sybe-Developer@users.noreply.github.com>
2023-05-27 12:29:16 +0530

    Update Func.js

--- JS Files/Func.js
+++ JS Files/Func.js
@@ -1,12 +1,4 @@
 //Link
-function link1() {
-window.open("https://www.mediafire.com/file/ldpqoy7od2wmb1l/Translator.exe/file");
-}
-
-function link2() {
-window.open("https://www.mediafire.com/file/lwvy72ix43k6eow/rps.exe/file");
-}
-
 function link3() {
 window.open("https://github.com/Sybe-Developer/Others");
 }
