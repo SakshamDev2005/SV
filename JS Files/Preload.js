df64877ad8d53767911b1d437753d3652d386bdd
Sybe-Developer <123803359+Sybe-Developer@users.noreply.github.com>
2023-06-03 13:00:59 +0530

    Update Preload.js

--- JS Files/Preload.js
+++ JS Files/Preload.js
@@ -1,23 +1,6 @@
 var preloader = document.getElementById('loading');
 
-function disableScrollAndPreload() {
-  // Disable scrolling
-  document.body.style.overflow = 'hidden';
-
-  // Start preloading here
-  // ...
-
-  // Call the function to re-enable scrolling after preloading is complete
-  preloadComplete();
-}
-
 function preloadComplete() {
-  // Restore scrolling
-  document.body.style.overflow = 'auto';
-
-  // Hide the preloader
   preloader.style.display = 'none';
 }
-
-// Call the function to disable scrolling and start preloading
-disableScrollAndPreload();
+  preloadComplete();
