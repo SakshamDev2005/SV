f90419aecc7f9d0d7447ee4b967e14c4d5473f76
Sybe-Developer <123803359+Sybe-Developer@users.noreply.github.com>
2023-06-21 13:11:33 +0530

    Update Preload.js

--- JS Files/Preload.js
+++ JS Files/Preload.js
@@ -1,6 +1,6 @@
 var preloader = document.getElementById('loading');
 var scroll = document.getElementById('pagee');
-scroll.style.overflow='hidden';
+scroll.style.overflow = "hidden";
 
 function preloadComplete() {
   preloader.style.display = 'none';
