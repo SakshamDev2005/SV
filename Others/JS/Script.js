a1a6b7b20668268a5bdd4228651d03e4472cffa5
Saksham <123803359+SakshamDev2005@users.noreply.github.com>
2024-04-04 23:27:53 +0530

    Add files via upload

---
+++ Others/JS/Script.js
@@ -0,0 +1,24 @@
+//Disable Shortcut Keys
+document.onkeydown = function (e) {
+    return false;
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Add a click event listener to all anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
